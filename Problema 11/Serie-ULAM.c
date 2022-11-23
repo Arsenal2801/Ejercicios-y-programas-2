9407a73eb78ec1fe73aa7f50713c69d9f98b269f
Arsenal2801 <campos.zeron.salvador28@gmail.com>
2022-11-22 23:19:08 -0600

    Serie-ULAM

--- Problema 11/Serie-ULAM.c
+++ Problema 11/Serie-ULAM.c
@@ -1,16 +1,18 @@
-
+//Importacion de librerias
 #include<stdio.h>
 
+//Declaracion de la funcion
 int main() {
+	// Declaracion de variables
 	int n;
-	printf("SERIE ULAM - INGRESE N�MERO : ");
-	scanf("%i",&n);
-	while ((n!=1)) {
-		printf("%i ",n);
-		if ((n%2)==0) {
-			n = n/2;
-		} else {
-			n = 3*n+1;
+	printf("SERIE ULAM - INGRESE N�MERO : ");  //Impresion de mensaje
+	scanf("%i",&n); //Lectura de variable
+	while ((n!=1)) { //Ciclo while
+		printf("%i ",n); //Impresion de variable
+		if ((n%2)==0) { //Condicion if si el residuo de la division es 0
+			n = n/2; //Operacion
+		} else { //Condicion else
+			n = 3*n+1; //Operacion
 		}
 	}
 	printf("\n");
